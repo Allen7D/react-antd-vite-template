04c9cd362969d6bc3eee623908dd046df54d9e59
Allen7D <462870781@qq.com>
2025-04-28 20:34:20 +0800

    feat: 添加 Tailwind CSS 和 Autoprefixer 支持

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# React + TypeScript + Vite 模板
+# React + TypeScript + Vite + AntD + Tailwind CSS
+
+## 技术方案
+[tailwindcss_3.4.17(英文文档)](https://v3.tailwindcss.com/docs/installation) - [tailwindcss_3.4.17(中文文档)](https://www.tailwindcss.cn/docs/guides/vite)
--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -1,5 +1,9 @@
 import "./index.less";
 
 export default function DashboardView() {
-  return <div className="dashboard-page page-content">Dashboard</div>;
+  return (
+    <div className="dashboard-page page-content">
+      <h1 className="text-3xl font-bold underline">Dashboard</h1>
+    </div>
+  );
 }
